98a642c0442b8e523865a214012b2dcf2226a926
Hanis <4571502+hanishassim@users.noreply.github.com>
2026-01-27 09:54:12 +0800

    fix: Apple standard button shape ios 18

--- Sources/Onboarding/Screens/Welcome/Apple/AppleBottomSection.swift
+++ Sources/Onboarding/Screens/Welcome/Apple/AppleBottomSection.swift
@@ -100,7 +100,6 @@ extension AppleBottomSection: View {
         
         return button
             .buttonStyle(.borderedProminent)
-            .buttonBorderShape(.capsule)
     }
 
     private func continueText() -> some View {
